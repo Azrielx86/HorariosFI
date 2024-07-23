cda200b116935cb2a24968905ab7cdfeb71efcb7
Azrielx86 <edgar1569@outlook.com>
2024-07-22 19:07:43 -0600

    Added tests

---
+++ HorariosFI.Test/ExcelExportTest.cs
@@ -0,0 +1,70 @@
+using HorariosFI.Core;
+
+namespace HorariosFI.Test;
+
+public class ExcelExportTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void NullInsertsTest()
+    {
+        var clases = new List<ClassModel>()
+        {
+            new()
+            {
+                Horario = "00:00",
+                Dias = "Lun, Mar",
+                Difficult = null,
+                Recommend = null,
+                Clave = 119,
+                Cupo = 40,
+                Grade = null,
+                Tipo = "T",
+                Gpo = 4,
+                Profesor = $"Profesor null",
+                Salon = "B550",
+                MisProfesoresUrl = null,
+                Vacantes = 40
+            }
+        };
+
+        ExcelExport.Export(119, "Null Values Test", clases);
+    }
+
+    [Test]
+    public void ValueRangesTest()
+    {
+        var clases = new List<ClassModel>();
+
+        for (var i = 0; i < 100; i++)
+        {
+            var difficult = i * 5 / 100.0;
+            var grade = i * 10 / 100.0;
+
+            var c = new ClassModel
+            {
+                Horario = "00:00",
+                Dias = "Lun, Mar",
+                Difficult = difficult,
+                Recommend = i,
+                Clave = 119,
+                Cupo = 40,
+                Grade = grade,
+                Tipo = "T",
+                Gpo = 4,
+                Profesor = $"Profesor {i}",
+                Salon = "B550",
+                MisProfesoresUrl = "http://localhost",
+                Vacantes = 40
+            };
+
+            clases.Add(c);
+        }
+
+        ExcelExport.Export(119, "Value Ranges Test", clases);
+    }
+}
