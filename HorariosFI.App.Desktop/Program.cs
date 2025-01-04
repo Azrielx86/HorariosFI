719b6c3fc61465f325c24554d0af6b04f54f5244
Azrielx86 <edgar1569@outlook.com>
2025-01-03 22:51:04 -0600

    Added icons, updated packages

--- HorariosFI.App.Desktop/Program.cs
+++ HorariosFI.App.Desktop/Program.cs
@@ -2,6 +2,8 @@
 
 using Avalonia;
 using Avalonia.ReactiveUI;
+using Projektanker.Icons.Avalonia;
+using Projektanker.Icons.Avalonia.MaterialDesign;
 
 namespace HorariosFI.App.Desktop;
 
@@ -16,9 +18,14 @@ class Program
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    {
+        IconProvider.Current
+            .Register<MaterialDesignIconProvider>();
+        
+        return AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace()
             .UseReactiveUI();
+    }
 }
